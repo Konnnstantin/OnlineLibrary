46c8fb1180048ae634b845be27c08140e39612f8
Konstantin <kurakin-1993@mail.ru>
2023-03-28 12:31:50 +0400

    Add project files.

---
+++ OnlineLibrary/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLibrary;
+{
+
+    UserRepository user = new UserRepository();
+    user.OnCreateUser();
+  //  user.SelectUsersId();
+
+    BookRepository bookRepository = new BookRepository();
+    var books = bookRepository.OnCreateModel();
+}
